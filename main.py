dd4f1e869da6a931c41c6b71e146aaff374c543b
my <37654068+leonatscarlett@users.noreply.github.com>
2018-03-22 15:03:37 +0300

    Port

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ config.initConfig()
 
 module = sys.modules[__name__]
 
+from intro_to_flask import app
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
+
 # Код настоятельно рекомендуется читать снизу вверх!
 
 #    _______        _
