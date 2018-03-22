b0bd20cbefc4d462f345c40ca66625641a113bc5
my <37654068+leonatscarlett@users.noreply.github.com>
2018-03-22 15:14:49 +0300

    Блять

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ config.initConfig()
 module = sys.modules[__name__]
 
 port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
 
 # Код настоятельно рекомендуется читать снизу вверх!
 
